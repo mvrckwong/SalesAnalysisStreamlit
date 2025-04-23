1aa7e7592f1881bcd8e85c82862e22e1e60fc69c
Maverick Wong <mvrck.wong@gmail.com>
2025-04-23 11:11:23 +0800

    Update logging.py

--- app/core/logging.py
+++ app/core/logging.py
@@ -5,7 +5,7 @@ PROJ_DIR = APP_DIR.parent
 
 DATA_DIR = PROJ_DIR / "data"
 TEST_DIR = PROJ_DIR / "test"
-CONT_DIR = PROJ_DIR / ".devcontainer"
+LOGS_DIR = PROJ_DIR / "logs"
 
 if __name__ == "__main__":
     None
